7320a61d87552cb017373e9e20430b4e91c4ffd8
ykarthik123 <106550205+ykarthik123@users.noreply.github.com>
2022-06-23 23:12:10 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","task4");
+$con = mysqli_connect("localhost","root","","blog");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
